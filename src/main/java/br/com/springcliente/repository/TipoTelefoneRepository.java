08591fc37c048ba38bf29e0e1b4fd10d00aaf691
Tiago Aguiar <tyago_tka48@hotmail.com>
2021-07-06 00:26:17 -0300

    chore: :construction: montando entidades tipo telefone uf. Adicionado lombok ao projeto

--- src/main/java/br/com/springcliente/repository/TipoTelefoneRepository.java
+++ src/main/java/br/com/springcliente/repository/TipoTelefoneRepository.java
@@ -1,11 +1,10 @@
 package br.com.springcliente.repository;
 
-import br.com.springcliente.enums.TipoTelefoneNome;
-import br.com.springcliente.model.TipoTelefone;
+import br.com.springcliente.enums.TipoTelefone;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
 import java.util.Optional;
 
-public interface TipoTelefoneRepository extends JpaRepositoryImplementation<TipoTelefone, Long> {
-    Optional<TipoTelefone> findByDescricao(TipoTelefoneNome descricao);
+public interface TipoTelefoneRepository extends JpaRepositoryImplementation<br.com.springcliente.model.TipoTelefone, Long> {
+    Optional<br.com.springcliente.model.TipoTelefone> findByDescricao(TipoTelefone descricao);
 }
--- src/main/java/br/com/springcliente/services/TipoTelefoneService.java
+++ src/main/java/br/com/springcliente/services/TipoTelefoneService.java
@@ -1,11 +1,11 @@
 package br.com.springcliente.services;
 
-import br.com.springcliente.enums.TipoTelefoneNome;
-import br.com.springcliente.model.TipoTelefone;
+import br.com.springcliente.enums.TipoTelefone;
+import br.com.springcliente.enums.TipoTelefoneDesc;
 
 import java.util.Optional;
 
 public interface TipoTelefoneService {
-    Optional<TipoTelefone> findByDescricao(TipoTelefoneNome descricao);
-    TipoTelefone save(TipoTelefone tipoTelefone);
+    Optional<br.com.springcliente.model.TipoTelefone> findByDescricao(TipoTelefoneDesc descricao);
+    br.com.springcliente.model.TipoTelefone save(br.com.springcliente.model.TipoTelefone tipoTelefone);
 }
